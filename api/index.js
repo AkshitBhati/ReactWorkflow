554d7134d719b0563ce1ebd1cacf98923bdcca02
Akshit Bhati <131599283+AkshitBhati@users.noreply.github.com>
2024-04-29 14:39:44 +0530

    preparing to deploy

--- api/index.js
+++ api/index.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 import connectDB from "./connectDB.js";
 import userRoutes from "./routes/user.routes.js";
 import workflowRoutes from "./routes/workflow.routes.js";
+import path from "path";
 
 dotenv.config();
 
@@ -11,6 +12,14 @@ dotenv.config();
 connectDB();
 
 const app = express();
+
+const __dirname = path.resolve()
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  });
 
 // Middleware
 app.use(express.json());
@@ -20,7 +29,6 @@ app.use(cookieParser());
 app.use("/api/auth", userRoutes);
 app.use("/api",workflowRoutes);
 
-// Error handling middleware 
 app.use((err, req, res, next) => {
     const statusCode = err.statusCode || 500;
     const message = err.message || "Internal server error";
@@ -32,7 +40,6 @@ app.use((err, req, res, next) => {
     });
 });
 
-// Start the server
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
